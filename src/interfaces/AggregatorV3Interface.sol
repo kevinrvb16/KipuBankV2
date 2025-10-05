8ee6b7dac2ee27f36a966a5813a3c029f9655949
kvbernal_meli <kevin.vbernal@mercadolivre.com>
2025-10-05 16:11:22 -0300

    Refactor AggregatorV3Interface: Remove unnecessary comments and streamline the interface definition

--- src/interfaces/AggregatorV3Interface.sol
+++ src/interfaces/AggregatorV3Interface.sol
@@ -1,36 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.30;
 
-/**
- * @title AggregatorV3Interface
- * @notice Interface for Chainlink Data Feeds
- * @dev This interface allows contracts to interact with Chainlink Price Feeds
- */
 interface AggregatorV3Interface {
-    /**
-     * @notice Returns the number of decimals present in the response value
-     */
     function decimals() external view returns (uint8);
 
-    /**
-     * @notice Returns a description of the price feed
-     */
     function description() external view returns (string memory);
 
-    /**
-     * @notice Returns the version of the aggregator
-     */
     function version() external view returns (uint256);
 
-    /**
-     * @notice Returns data about a specific round
-     * @param _roundId The round ID to retrieve the data for
-     * @return roundId The round ID
-     * @return answer The price
-     * @return startedAt Timestamp of when the round started
-     * @return updatedAt Timestamp of when the round was updated
-     * @return answeredInRound The round ID in which the answer was computed
-     */
     function getRoundData(uint80 _roundId)
         external
         view
@@ -42,14 +19,6 @@ interface AggregatorV3Interface {
             uint80 answeredInRound
         );
 
-    /**
-     * @notice Returns the latest round data
-     * @return roundId The round ID
-     * @return answer The price
-     * @return startedAt Timestamp of when the round started
-     * @return updatedAt Timestamp of when the round was updated
-     * @return answeredInRound The round ID in which the answer was computed
-     */
     function latestRoundData()
         external
         view
